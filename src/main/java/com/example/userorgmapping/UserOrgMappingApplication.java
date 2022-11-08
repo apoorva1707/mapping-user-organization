bc03b1a6f662211418dbc2f638b0c59ae9f49ffd
apoorva1707 <47132379+apoorva1707@users.noreply.github.com>
2022-11-08 12:46:11 +0530

    added pom.xml

---
+++ src/main/java/com/example/userorgmapping/UserOrgMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.userorgmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOrgMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserOrgMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userorgmapping/UserOrgMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userorgmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOrgMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
